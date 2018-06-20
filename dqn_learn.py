17a477904956ecb9d3b191d1fded9596ec980bc2
TheMagament <40318274+TheMagament@users.noreply.github.com>
2018-06-21 00:13:31 +0300

    fix2

--- dqn_learn.py
+++ dqn_learn.py
@@ -181,8 +181,7 @@ def dqn_learing(
         ### 1. Check stopping criterion
         if stopping_criterion is not None and stopping_criterion(env):
             break
-
-		if (t>1000000):
+        if (t>1000000):
             break
         ### 2. Step the env and store the transition
         
